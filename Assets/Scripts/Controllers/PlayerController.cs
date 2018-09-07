88231cbbed47e241116d744a0632c7788887738b
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-07 01:19:07 -0400

    Adding a player controller

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class PlayerController : MonoBehaviour {
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    #region main variables
+    public bool blockContrllerInput = false;
+    #endregion main variables
+
+    #region monobehaiovur methods
+    private void Update()
+    {
+        if (blockContrllerInput)
+        {
+            return;
+        }
+        float horizontalInput = Input.GetAxisRaw(HORIZONTAL);
+        float verticalInput = Input.GetAxisRaw(VERTICAL);
+
+    }
+    #endregion monobehaviour methods
+}
