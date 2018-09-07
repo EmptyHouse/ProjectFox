10a24becafa9a297999227ce0c46f116a4384808
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-07 01:12:20 -0400

    Began movement script and added placeholder kanna

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    [Tooltip("The maximum speed of the character")]
+    public float maxCharacterSpeed;
+
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Updates the movement speed of the character
+    /// </summary>
+    private void UpdateMovementSpeed()
+    {
+
+    }
+    
+    private void Jump()
+    {
+
+    }
+}
