008b1690d316b57674e2491b71a6c63c3fe5143a
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-08 03:13:58 -0400

    Creating a splash screen and UI components

---
+++ Assets/Scripts/UIScripts/SelectableUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for selectable UI objects, such as sliders, buttons, etc
+/// </summary>
+public abstract class SelectableUI : MonoBehaviour {
+    public enum Directions
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    private const float JOYSTICK_ACTIVATION_THRESHOLD = .7f;
+
+    public SelectableUI northUINode;
+    public SelectableUI southUINode;
+    public SelectableUI eastUINode;
+    public SelectableUI westUINode;
+
+    public bool isNodeDisabled = false;
+
+    public SelectableUI GetNodeInDirection(Directions directionToRetrieveNode)
+    {
+        switch (directionToRetrieveNode)
+        {
+            case Directions.North:
+                return northUINode;
+            case Directions.South:
+                return southUINode;
+            case Directions.East:
+                return eastUINode;
+            case Directions.West:
+                return westUINode;
+        }
+
+        return null;
+    }
+
+    public abstract void DisplayNodeAsDisabled();
+
+    public abstract void VisuallyUpdateNode();
+
+    public abstract void UpdateNodeValue();
+}
