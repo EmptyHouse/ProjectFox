f18baaa8a86d509ac326da2e8e2c4fe2b016b298
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-08 15:35:40 -0400

    Starting development of combat manager

--- Assets/Scripts/CombatScripts/CombatManager.cs
+++ Assets/Scripts/CombatScripts/CombatManager.cs
@@ -3,5 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatManager : MonoBehaviour {
+    public Transform[] allEnemyCombatSpawnPoints;
+    public Transform[] allPlayerPartyCombatSpawnPoints;
 
+    private void OnDrawGizmos()
+    {
+        float spherePointRadius = .5f;
+        Color enemyColor = Color.red;
+        Color playerColor = Color.blue;
+
+        foreach (Transform t in allEnemyCombatSpawnPoints)
+        {
+            Gizmos.color = enemyColor;
+            Gizmos.DrawSphere(t.position, spherePointRadius);
+        }
+        foreach (Transform t in allPlayerPartyCombatSpawnPoints)
+        {
+            Gizmos.color = playerColor;
+            Gizmos.DrawSphere(t.position, spherePointRadius);
+        }
+    }
 }
