46f2e2c693ed7d599621fade2b25f26c41fa323f
Ryan Andersen <ryan.andersen05@gmail.com>
2018-09-08 14:09:17 -0400

    Making updates to the combat mechanics

---
+++ Assets/Scripts/CombatScripts/CombatCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatCharacter : MonoBehaviour {
+    
+
+    [Header("Default Stats")]
+    public float maxHealthPoints;
+    public float maxSpecialPoints;
+    public float armorClass;
+    [Range(0f, 1f)]
+    public float evasionRate;
+
+    public float currentHealthPoints { get; private set; }
+    public float currentSpecialPoints { get; private set; }
+
+    private void Start()
+    {
+        currentHealthPoints = maxHealthPoints;
+        currentSpecialPoints = maxSpecialPoints;
+    }
+
+
+
+    public virtual void TakeDamage(CombatCharacter characterThatDamagedMe, float damageToTake)
+    {
+        currentHealthPoints -= damageToTake;
+    }
+
+    public virtual void OnCharacterDied(CombatCharacter characterThatKilledMe)
+    {
+
+    }
+}
